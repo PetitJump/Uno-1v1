d673360187259f442020ecda65cd096c6a2513e7
PetitJump <killianbonnin7@gmail.com>
2025-09-16 12:11:31 +0200

    Changement de la class Jeu et __init__

--- main.py
+++ main.py
@@ -2,31 +2,26 @@ import json, os
 from random import sample
 
 class Jeu:
-    def __init__(self, main1: list[list], main2: list[list], table: list[str, int]):
-        self.main1 = main1
-        self.main2 = main2
-        self.table = table
+    def __init__(self):
+        clear()
+        self.table = sample(total_cartes, k=1)[0]
+        self.table = Table(self.table[0][0], self.table[0][1] )
+        print("Bienvenue au Uno")
+        self.pseudo1 = input("Choix pseudo 1 : ")
+        clear()
+        print("Bienvenue au Uno")
+        self.pseudo2 = input("Choix pseudo 2 : ")
+        clear()
+        print(f"{self.pseudo1}, voici vos cartes :")
+        self.main_actuel1 = sample(total_cartes, k=7)
+        print(self.main_actuel1)
+        input("Appuyer sur entré pour continuer")
+        clear()
+        print(f"{self.pseudo2}, voici vos cartes")
+        self.main_actuel2 = sample(total_cartes, k=7)
+        print(self.main_actuel2)
+        input("Appuyer sur entré pour continuer")
 
-    def debut(self):
-        clear()
-        table = sample(total_cartes, k=1)[0]
-        self.table = Table(table[0][0], table[0][1] )
-        print("Bienvenue au Uno")
-        pseudo1 = input("Choix pseudo 1 : ")
-        clear()
-        print("Bienvenue au Uno")
-        pseudo2 = input("Choix pseudo 2 : ")
-        clear()
-        print(f"{pseudo1}, voici vos cartes :")
-        main_actuel1 = sample(total_cartes, k=7)
-        print(main_actuel1)
-        print(f"{pseudo2}, voici vos cartes")
-        input("Appuyer sur entré pour continuer")
-        clear()
-        main_actuel2 = sample(total_cartes, k=7)
-        print(main_actuel2)
-        input("Appuyer sur entré pour continuer")
-    
     def cartes_posables(self, main):
         rendu = []
         for c in main:
@@ -35,7 +30,7 @@ class Jeu:
         return rendu
     
     def run(self):
-        while len(self.main1) > 0 and len(self.main2) > 0:
+        while len(self.main_actuel1) > 0 and len(self.main_actuel2) > 0:
             print("")
         
 
@@ -58,4 +53,4 @@ with open('data.json', 'r', encoding='utf-8') as f:
     total_cartes = json.load(f)
 
 Partie_en_cour = Jeu()
-Partie_en_cour.debut()
+Partie_en_cour.run()
