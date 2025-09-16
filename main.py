e511b382356e7dec2e5ced6f3607102251109115
PetitJump <killianbonnin7@gmail.com>
2025-09-16 18:30:50 +0200

    2ème push (version non finis)

--- main.py
+++ main.py
@@ -1,52 +1,89 @@
-import json, os
+import json, os, random
 from random import sample
-
-class Jeu:
-    def __init__(self):
-        clear()
-        self.table = sample(total_cartes, k=1)[0]
-        self.table = Table(self.table[0][0], self.table[0][1] )
-        print("Bienvenue au Uno")
-        self.pseudo1 = input("Choix pseudo 1 : ")
-        clear()
-        print("Bienvenue au Uno")
-        self.pseudo2 = input("Choix pseudo 2 : ")
-        clear()
-        print(f"{self.pseudo1}, voici vos cartes :")
-        self.main_actuel1 = sample(total_cartes, k=7)
-        print(self.main_actuel1)
-        input("Appuyer sur entré pour continuer")
-        clear()
-        print(f"{self.pseudo2}, voici vos cartes")
-        self.main_actuel2 = sample(total_cartes, k=7)
-        print(self.main_actuel2)
-        input("Appuyer sur entré pour continuer")
-
-    def cartes_posables(self, main):
-        rendu = []
-        for c in main:
-            if c[0] == self.table[0] or c[1] == self.table[1]:
-                rendu.append(c)
-        return rendu
-    
-    def run(self):
-        while len(self.main_actuel1) > 0 and len(self.main_actuel2) > 0:
-            print("")
-        
 
 class Table:
     def __init__(self, couleur, numero):
         self.couleur = couleur
         self.numero = numero
+
     def __repr__(self):
-        return [self.couleur, self.numero]
+        return f"[{self.couleur}, {self.numero}]"
 
 class Main:
+    def __init__(self, main, pseudo):
+        self.main = main
+        self.pseudo = pseudo
+        self.table_actu = ["couleur", 0]
+
+    def choix(self, table: list[str, int]):
+        carte_dispo = self.cartes_posables(self.main, table) #carte_dispo = list
+        print("")
+        if len(carte_dispo) > 0:
+            print("Voici les cartes que vous pouvez jouer :")
+            for i in range(len(carte_dispo)):
+                print(f"{i + 1} : {carte_dispo[i]}")
+            choix = int(input("Votre choix : "))
+            carte_jouer = carte_dispo[choix - 1]
+            self.main.remove(carte_jouer)
+            self.table_actu = carte_jouer
+        else:
+            self.table_actu = table
+            print("Vous n'avez pas de carte possible à jouer.")
+            print("Vous piochez donc une carte")
+            nouvelle_carte = random.choice(total_cartes)
+            print(f"Carte piocher : {nouvelle_carte}")
+            self.main.append(nouvelle_carte) 
+
+    def cartes_posables(self, main, table):
+        rendu = []
+        for c in main:
+            if c[0] == table[0] or c[1] == table[1]:
+                rendu.append(c)
+        return rendu
+
+class Jeu:
     def __init__(self):
-        pass
+        clear()
+        self.table = sample(total_cartes, k=1)[0]
+        self.table = Table(self.table[0], self.table[1] )
+
+        main_actuel1 = sample(total_cartes, k=7)
+
+        main_actuel2 = sample(total_cartes, k=7)
+
+        print("Bienvenue au Uno")
+        pseudo1 = input("Choix pseudo 1 : ")
+        clear()
+        print("Bienvenue au Uno")
+        pseudo2 = input("Choix pseudo 2 : ")
+        clear()
+        print(f"{pseudo1}, voici vos cartes :")
+        print(main_actuel1)
+        input("Appuyer sur entré pour continuer")
+        clear()
+        print(f"{pseudo2}, voici vos cartes")
+        print(main_actuel2)
+        input("Appuyer sur entré pour continuer")
+
+        self.J1 = Main(main_actuel1, pseudo1)
+        self.J2 = Main(main_actuel2, pseudo2)
+
+    
+    def run(self):
+        while len(self.J1.main) > 0 and len(self.J2.main) > 0:
+            #clear()
+            print(f"{self.J1.pseudo}, c'est à vous de jouer. Voici vos cartes :")
+            print(self.J1.main)
+            print(f"La carte actuelement sur la table : {self.table}")
+            self.J1.choix([self.table.couleur, self.table.numero])
+            self.table.couleur = self.J1.table_actu[0]
+            self.table.numero = self.J1.table_actu[1]
+            print(self.table.numero)
+            input("Continuer ?")
+
+
 
 def clear():
-    """Efface le terminal (Windows ou Linux/Mac)"""
     os.system('cls' if os.name == 'nt' else 'clear')
 
 with open('data.json', 'r', encoding='utf-8') as f:
